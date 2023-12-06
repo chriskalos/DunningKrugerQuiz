e2bb2c1dfb9ae446fa8990228c11a89a8c7112ec
Chris <chriskalos658@gmail.com>
2023-12-06 21:09:28 +0200

    Screw the web stuff, but I'm going to commit it

--- script.js
+++ script.js
@@ -1,56 +1,94 @@
-$(document).ready(function() {
-    const questions = [
-        "Question 1", "Question 2", "Question 3", "Question 4", "Question 5", 
-        "Question 6", "Question 7", "Question 8", "Question 9", "Question 10"
-    ];
-    const answers = ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10"];
-
-    // Dynamically generate the question fields
-    for (let i = 0; i < questions.length; i++) {
-        $('#questions-container').append(
-            '<div class="form-group question">' +
-            '<label for="question' + i + '">' + questions[i] + ':</label>' +
-            '<input type="text" id="question' + i + '" class="form-control">' +
-            '</div>'
-        );
-    }
-
-    // Handle the submit button click event
-    $('#submit-btn').click(function() {
-        let correctAnswers = 0;
-        for (let i = 0; i < answers.length; i++) {
-            if ($('#question' + i).val().trim() === answers[i]) {
-                correctAnswers++;
-            }
-        }
-
-        const userChoice = parseInt($('#userChoice').val());
-        const score = calculateScore(userChoice, correctAnswers);
-        $('#result').text('Your score is: ' + score);
+$(document).ready(function () {
+    // Logic for initial choice submission
+    $('#initial-choice-form').submit(function (event) {
+        event.preventDefault();
+        const choice = $('input[name="choice"]:checked').val();
+        localStorage.setItem('userChoice', choice);
+        window.location.href = 'quiz.html'; // Redirect to the quiz page
     });
 
-    // Scoring logic
-    function calculateScore(choice, correctAnswers) {
-        if (choice === 1 || choice === 2) {
-            if (correctAnswers > 3) {
-                return 40 + (correctAnswers - 4) * 4;  // Score within 40-60
-            } else {
-                return correctAnswers;  // Score within 1-10
-            }
-        } else if (choice === 3) {
-            if (correctAnswers < 2) {
-                return correctAnswers;
-            } else if (correctAnswers < 8) {
-                return 41 + (correctAnswers - 2) * 6;  // Avoids 20-40 range
-            } else {
-                return 89 + correctAnswers;  // High scores for good performance
-            }
-        } else if (choice === 4 || choice === 5) {
-            if (correctAnswers < 5) {
-                return 20 + correctAnswers * 4;  // Score within 20-40
-            } else {
-                return 60 + (correctAnswers - 5) * 8;  // Score within 60-100
-            }
+    // Check if on the quiz page
+    if (window.location.pathname.endsWith('quiz.html')) {
+        // Quiz questions and options
+        const questions = [
+            { question: "Question 1", options: ["Option A", "Option B", "Option C", "Option D"], answer: "Option A" },
+            { question: "Question 2", options: ["Option A", "Option B", "Option C", "Option D"], answer: "Option A" },
+            { question: "Question 3", options: ["Option A", "Option B", "Option C", "Option D"], answer: "Option A" },
+            { question: "Question 4", options: ["Option A", "Option B", "Option C", "Option D"], answer: "Option A" },
+            // ... Add more questions in similar format
+            // { question: "Question N", options: ["Option A", "Option B", "Option C", "Option D"], answer: "Option A" }
+        ];
+
+        // Dynamically generate quiz questions
+        questions.forEach((q, index) => {
+            let optionsHtml = q.options.map((option, i) => {
+                return `<div class="form-check">
+                            <input class="form-check-input" type="radio" name="question${index}" id="question${index}option${i}" value="${option}">
+                            <label class="form-check-label" for="question${index}option${i}">${option}</label>
+                        </div>`;
+            }).join('');
+
+            $('#questions-container').append(
+                `<div class="mb-4">
+                    <p>${q.question}</p>
+                    ${optionsHtml}
+                </div>`
+            );
+        });
+
+        // Handle quiz form submission
+        $('#quiz-form').submit(function (event) {
+            event.preventDefault();
+            let correctAnswers = 0;
+            questions.forEach((q, index) => {
+                let selectedOption = $(`input[name="question${index}"]:checked`).val();
+                if (selectedOption === q.answer) {
+                    correctAnswers++;
+                }
+            });
+
+            const userChoice = parseInt(localStorage.getItem('userChoice'));
+            const score = calculateScore(userChoice, correctAnswers);
+            localStorage.setItem('quizScore', score);
+            window.location.href = 'results.html'; // Redirect to results page
+        });
+
+        // Display the score on results.html
+        if (window.location.pathname.includes('results.html')) {
+            const score = localStorage.getItem('quizScore');
+            $('#score-display').text('Your Score: ' + score);
         }
     }
 });
+
+// Function to calculate score based on choice and correct answers
+function calculateScore(choice, correctAnswers) {
+    if (choice === 1 || choice === 2) {
+        if (correctAnswers > 3) {
+            return 40 + (correctAnswers - 4) * 4; // Ensures score is within 40-60
+        } else {
+            return correctAnswers; // Ensures score is within 1-10
+        }
+    } else if (choice === 3) {
+        if (correctAnswers < 2) {
+            return correctAnswers;
+        } else if (correctAnswers < 8) {
+            return 41 + (correctAnswers - 2) * 6; // Avoids 20-40 range
+        } else {
+            return 89 + correctAnswers; // Ensures high scores for good performance
+        }
+    } else if (choice === 4 || choice === 5) {
+        if (correctAnswers < 5) {
+            return 20 + correctAnswers * 4; // Ensures score is within 20-40
+        } else {
+            return 60 + (correctAnswers - 5) * 8; // Ensures score is within 60-100
+        }
+    }
+}
+
+// Function to display result dot
+function displayResultDot(score) {
+    const lineLength = $('#result-line').width();
+    const position = (lineLength * score) / 100 - 5; // Adjust for dot size
+    $('#result-dot').css('left', position + 'px').show(); // Show the dot
+}
